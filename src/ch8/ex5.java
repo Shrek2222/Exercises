b6a225279ea0014698a8e0e8c26b00defa74b0cb
ShreshthAwadhiya <ShreshthAwadhiya@192.168.2.122>
2018-07-12 19:54:44 -0400

    tatest

--- src/ch8/ex5.java
+++ src/ch8/ex5.java
@@ -1,5 +1,48 @@
 package ch8;
-
+import java.util.Scanner;
 public class ex5 {
 
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double[][] one = new double[3][3];
+        double[][] two = new double[3][3];
+            
+        System.out.println("Enter matrix1  in the form of a (3x3): ");
+        for (int i = 0; i < one.length; i++) {
+            for (int j = 0; j < two[i].length; j++) {
+                one[i][j] = in.nextDouble();
+            }
+        }
+
+        System.out.println("Enter matrix2 (3x3): ");
+        for (int i = 0; i < two.length; i++) {
+            for (int j = 0; j < two[i].length; j++) {
+                two[i][j] = in.nextDouble();
+            }
+        }
+
+        double[][] sum = addMatrix(one, two);
+        System.out.println("The matricies added are as follows: ");
+
+        for (int i = 0; i < one.length; i++) {
+            System.out.printf("%.1f %.1f %.1f  %s  %.1f %.1f %.1f  %s  %.1f %.1f %.1f%n",
+                one[i][0],one[i][1], one[i][2],
+                i == 1 ? "+" : " ",
+                one[i][0], one[i][1],two[i][2],
+                i == 1 ? "=" : " ",
+                sum[i][0], sum[i][1], sum[i][2]);
+        }
+    }
+
+    public static double[][] addMatrix(double[][] a, double[][] b) {
+        double[][] c = new double[a.length][a.length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return c;
+    }
 }
+
--- src/ch8/ex8.java
+++ src/ch8/ex8.java
@@ -1,5 +1,49 @@
 package ch8;
-
+import java.util.Scanner;
+import java.lang.Math;
 public class ex8 {
+	
 
+	
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter the number of points: ");
+			int point = input.nextInt();
+
+			
+			double[][] points = new double[point][2];
+			System.out.print("Enter " + point + " points: ");
+			for (int i = 0; i < points.length; i++) {
+				points[i][0] = input.nextDouble();
+				points[i][1] = input.nextDouble();
+			}
+
+			int p1 = 0, p2 = 1; 
+			double shortdistance = (points[p1][0], points[p1][1],
+				points[p2][0], points[p2][p1]); 
+
+			
+			for (int i = 0; i < points.length; i++) {
+				for (int j = i + 1; j < points.length; j++) {
+					// double distance =??? (points[i][0],[i][1],
+						// points[j][0], points[j][1]); 
+
+					// if (shortdistance > distance) {
+						p1 = i; 
+						p2 = j; 
+						shortdistance = distance; 
+					}
+				}
+			}
+
+			
+			
+				}
+			}
+			System.out.println("The distance is " + shortdistance);
+		}
+
+	
+		}
+	
 }
