d14d01364e06f496c0ea61cd795ec60e06963978
ShreshthAwadhiya <ShreshthAwadhiya@192.168.2.122>
2018-07-11 20:08:38 -0400

    ch 07

---
+++ src/ch7/ArrayProblem2.java
@@ -0,0 +1,17 @@
+package ch7;
+
+public class ArrayProblem2 {
+	
+	      public static void main(String[] args) {
+	        int[] list1 = {1, 2, 3};
+	        int[] list2 = list1;
+	        list2[1] = 10;
+	        System.out.println(list1[1]);
+	        double daaa[][][] = new double[3][][];
+	        double d = 100.0;
+	        double[][] daa = new double[1][1];
+	        
+
+	      }
+	    
+}
