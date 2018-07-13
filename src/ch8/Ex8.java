7e5e7f41e783d0752801d5f66df917124ea39c61
ShreshthAwadhiya <ShreshthAwadhiya@192.168.2.122>
2018-07-13 18:46:34 -0400

    test

--- src/ch8/Ex8.java
+++ src/ch8/Ex8.java
@@ -2,6 +2,8 @@ package ch8;
 import java.util.Scanner;
 import java.lang.Math;
 public class Ex8 {
+	String s;
+	
 	public static double[][] readData(){
 		Scanner input = null;
 		double[][] returnVal;
@@ -14,6 +16,7 @@ public class Ex8 {
 			for (int i = 0; i < returnVal.length; i++) {
 				returnVal[i][0] = input.nextDouble();
 				returnVal[i][1] = input.nextDouble();
+				System.out.println("Next point");
 			}
 		}finally {
 			input.close();
@@ -23,36 +26,34 @@ public class Ex8 {
 	public static double getDistance(
 			double x1, double y1, double x2, double y2)
 	{
-			return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		}
+		return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	}
 	private double findDistance(double x1, double y1, double x2, double y2) {
 		return 0.0;
 	}
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the number of points you want to compare:");
-		int numberOfPoints = input.nextInt();
-		double[][] points = new double[numberOfPoints][2];
-		System.out.print("Enter " + numberOfPoints + " points: ");
-		for (int i = 0; i < points.length; i++) {
-			points[i][0] = input.nextDouble();
-			points[i][1] = input.nextDouble();
-		
-		}
+
+
+
+		double[][] points = readData();
+		double minDistance = Double.MAX_VALUE;
 		for (int i = 0; i < points.length; i++) {
 			for (int j = i + 1; j < points.length; j++) {
 				double distance = getDistance(points[i][0], points[i][1],
-					points[j][0], points[j][1]);
+						points[j][0], points[j][1]);
+				if (minDistance > distance) {
+					minDistance = distance;
 
-				 
 				}
 			}
 		}
-		
+		System.out.println("The minimum distance is "+minDistance);
+	}
 
-			
-		
+
+
+
 }
 
 
