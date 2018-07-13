cb24d4452f443e17d751ea08faeda3d8392558fa
awadhiya <pawadhiya@gmail.com>
2018-07-12 20:23:00 -0400

    fixed typo

--- src/ch8/Ex8.java
+++ src/ch8/Ex8.java
@@ -1,49 +1,57 @@
 package ch8;
 import java.util.Scanner;
 import java.lang.Math;
-public class ex8 {
-	
-
-	
-		public static void main(String[] args) {
-			Scanner input = new Scanner(System.in);
+public class Ex8 {
+	public static double[][] readData(){
+		Scanner input = null;
+		double[][] returnVal;
+		try {
+			input = new Scanner(System.in);
 			System.out.print("Enter the number of points: ");
 			int point = input.nextInt();
-
-			
-			double[][] points = new double[point][2];
+			returnVal = new double[point][2];
 			System.out.print("Enter " + point + " points: ");
-			for (int i = 0; i < points.length; i++) {
-				points[i][0] = input.nextDouble();
-				points[i][1] = input.nextDouble();
+			for (int i = 0; i < returnVal.length; i++) {
+				returnVal[i][0] = input.nextDouble();
+				returnVal[i][1] = input.nextDouble();
 			}
+		}finally {
+			input.close();
+		}
+		return returnVal;
+	}
+	private double findDistance(double x1, double y1, double x2, double y2) {
+		return 0.0;
+	}
 
-			int p1 = 0, p2 = 1; 
-			double shortdistance = (points[p1][0], points[p1][1],
+	public static void main(String[] args) {
+		double[][] points = readData();
+		int p1 = 0, p2 = 1; 
+		double shortdistance = (points[p1][0], points[p1][1],
 				points[p2][0], points[p2][p1]); 
 
-			
-			for (int i = 0; i < points.length; i++) {
-				for (int j = i + 1; j < points.length; j++) {
-					// double distance =??? (points[i][0],[i][1],
-						// points[j][0], points[j][1]); 
 
-					// if (shortdistance > distance) {
-						p1 = i; 
-						p2 = j; 
-						shortdistance = distance; 
-					}
-				}
+		for (int i = 0; i < points.length; i++) {
+			for (int j = i + 1; j < points.length; j++) {
+				// double distance =??? (points[i][0],[i][1],
+				// points[j][0], points[j][1]); 
+
+				// if (shortdistance > distance) {
+				p1 = i; 
+				p2 = j; 
+				shortdistance = distance; 
 			}
-
-			
-			
-				}
-			}
-			System.out.println("The distance is " + shortdistance);
 		}
+	}
 
-	
-		}
-	
+
+
+}
+}
+System.out.println("The distance is " + shortdistance);
+}
+
+
+}
+
 }
