449e0cb5ecf26d000ecba15a054fefa40ae40222
ShreshthAwadhiya <ShreshthAwadhiya@192.168.2.122>
2018-07-12 21:19:28 -0400

    test

--- src/ch8/Ex8.java
+++ src/ch8/Ex8.java
@@ -20,38 +20,39 @@ public class Ex8 {
 		}
 		return returnVal;
 	}
+	public static double getDistance(
+			double x1, double y1, double x2, double y2)
+	{
+			return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		}
 	private double findDistance(double x1, double y1, double x2, double y2) {
 		return 0.0;
 	}
 
 	public static void main(String[] args) {
-		double[][] points = readData();
-		int p1 = 0, p2 = 1; 
-		double shortdistance = (points[p1][0], points[p1][1],
-				points[p2][0], points[p2][p1]); 
-
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number of points you want to compare:");
+		int numberOfPoints = input.nextInt();
+		double[][] points = new double[numberOfPoints][2];
+		System.out.print("Enter " + numberOfPoints + " points: ");
+		for (int i = 0; i < points.length; i++) {
+			points[i][0] = input.nextDouble();
+			points[i][1] = input.nextDouble();
+		
+		}
 		for (int i = 0; i < points.length; i++) {
 			for (int j = i + 1; j < points.length; j++) {
-				// double distance =??? (points[i][0],[i][1],
-				// points[j][0], points[j][1]); 
+				double distance = getDistance(points[i][0], points[i][1],
+					points[j][0], points[j][1]);
 
-				// if (shortdistance > distance) {
-				p1 = i; 
-				p2 = j; 
-				shortdistance = distance; 
+				 
+				}
 			}
 		}
-	}
+		
 
-
-
-}
-}
-System.out.println("The distance is " + shortdistance);
+			
+		
 }
 
 
-}
-
-}
