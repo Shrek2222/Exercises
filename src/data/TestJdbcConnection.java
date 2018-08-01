1887c075b25915161d3a81c31be092bc3b8e58b0
awadhiya <pawadhiya@gmail.com>
2018-08-01 14:46:10 -0400

    JDBC

---
+++ src/data/TestJdbcConnection.java
@@ -0,0 +1,20 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJdbcConnection {
+	
+public static void main(String args[]) {
+	String connectionUrl = "jdbc:sqlserver://localhost;instanceName=MSSQLSERVER1;"+  
+			  "databaseName=sonar;user=sonar_user;password=sonar";  
+			try {
+				Connection con = DriverManager.getConnection(connectionUrl);
+				System.out.println(con);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+}
+}
